1b17da92ef46ab71bb4b650ca83247d90c457dd7
Ahill Pranav M <itisahill.m@gmail.com>
2025-01-27 12:52:41 +0530

    Uses two for loops to print even numbers first and odd numbers at last

---
+++ Parking_management.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void sortArraybyParity(int *nums,int numSize)
+{
+    for (int i=0;i<numSize;i++)
+    {
+        if(nums[i]%2==0)
+        {
+            printf("%d\t",nums[i]);
+            
+        }
+    }
+    for (int i=0;i<numSize;i++)
+    {
+        if(nums[i]%2!=0)
+        {
+            printf("%d\t",nums[i]);
+            
+        }
+}
+ printf("\n");
+}
+int main()
+{
+    int numsize;
+    printf("ENter number of tickets");
+    scanf("%d",&numsize);
+    int nums[numsize];
+    for(int i=0;i<numsize;i++)
+    {
+    scanf("%d",&nums[i]);
+    }
+    sortArraybyParity(nums,numsize);
+}
